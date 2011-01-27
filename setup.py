b7911179fd8cdcdf78d963e481f8b2d7c174c2b0
Zach Denton <zacharydenton@gmail.com>
2011-01-27 14:13:57 -0500

    distutils support

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''
+Installer script for whatbetter.
+'''
+
+from distutils.core import setup
+
+setup (
+    name = "whatbetter",
+    description = "Automatically transcode and upload FLACs on What.CD.",
+    author = 'Zach Denton',
+    author_email = 'zacharydenton@gmail.com',
+    version = '1.0',
+    url = 'http://zacharydenton.com/code/whatbetter/',
+    py_modules = ['mediafile',
+                'transcode',
+                'whatbrowser',
+               ],
+    scripts = ['whatbetter'],
+    requires = ['mutagen',
+                'mechanize',
+                'argparse',
+                'BeautifulSoup',
+                'lxml']
+)
