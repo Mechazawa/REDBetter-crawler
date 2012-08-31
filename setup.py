5f050e0851851fba7436ec74ceffdee497515732
Zach Denton <zacharydenton@gmail.com>
2012-08-31 14:44:16 -0400

    Use proper Python formatting.

--- setup.py
+++ setup.py
@@ -5,20 +5,24 @@ Installer script for whatbetter.
 
 from setuptools import setup
 
-setup (
+setup(
     name = "whatbetter",
     description = "Automatically transcode and upload FLACs on What.CD.",
     author = 'Zach Denton',
     author_email = 'zacharydenton@gmail.com',
     version = '1.0',
     url = 'http://github.com/zacharydenton/whatbetter',
-    py_modules = ['mediafile',
-                  'transcode',
-                  'whatapi'],
+    py_modules = [
+        'mediafile',
+        'transcode',
+        'whatapi'
+    ],
     scripts = ['whatbetter'],
-    install_requires = ['mutagen',
-                'mechanize',
-                'argparse',
-                'ordereddict',
-                'requests']
+    install_requires = [
+        'mutagen',
+        'mechanize',
+        'argparse',
+        'ordereddict',
+        'requests'
+    ]
 )
