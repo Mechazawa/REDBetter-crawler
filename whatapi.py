674e13ec786332d921240e073b2b7f0112c520f0
Zach Denton <zacharydenton@gmail.com>
2012-08-31 18:08:58 -0400

    Use the standard HTML entity decoder.

--- whatapi.py
+++ whatapi.py
@@ -5,7 +5,7 @@ import json
 import time
 import requests
 import mechanize
-import htmlentitydefs
+import HTMLParser
 from cStringIO import StringIO
 
 headers = {
@@ -160,35 +160,4 @@ class WhatAPI:
         response = self.session.post(url, data=data, headers=dict(headers))
 
 def unescape(text):
-   """Removes HTML or XML character references 
-      and entities from a text string.
-      keep &amp;, &gt;, &lt; in the source code.
-   from Fredrik Lundh
-   http://effbot.org/zone/re-sub.htm#unescape-html
-   """
-   def fixup(m):
-      text = m.group(0)
-      if text[:2] == "&#":
-         # character reference
-         try:
-            if text[:3] == "&#x":
-               return unichr(int(text[3:-1], 16))
-            else:
-               return unichr(int(text[2:-1]))
-         except ValueError:
-            pass
-      else:
-         # named entity
-         try:
-            if text[1:-1] == "amp":
-               text = "&amp;amp;"
-            elif text[1:-1] == "gt":
-               text = "&amp;gt;"
-            elif text[1:-1] == "lt":
-               text = "&amp;lt;"
-            else:
-               text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-         except KeyError:
-            pass
-      return text # leave as is
-   return re.sub("&#?\w+;", fixup, text)
+    return HTMLParser.HTMLParser().unescape(text)
