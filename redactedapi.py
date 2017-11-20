c7e3ff56d7c21a6144cfab0bc369de8398515d7c
Grant Bevis <grant@bevis.co>
2017-11-20 12:46:44 +0000

    Cosmetic changes of PTH to RED

--- redactedapi.py
+++ redactedapi.py
@@ -11,7 +11,7 @@ from cStringIO import StringIO
 headers = {
     'Connection': 'keep-alive',
     'Cache-Control': 'max-age=0',
-    'User-Agent': 'PTHBetter crawler',
+    'User-Agent': 'REDBetter crawler',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Encoding': 'gzip,deflate,sdch',
     'Accept-Language': 'en-US,en;q=0.8',
